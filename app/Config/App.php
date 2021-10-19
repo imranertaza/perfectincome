cf535a48e7da43bf28c6e3b6e75db59b6217e368
Syed Imran Ertaza <imranertaza12@gmail.com>
2021-10-19 10:51:51 +0600

    Added config file to dev branch

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/starsfair/public/';
+    public $baseURL = 'http://perfectincome.dns/';
 
     /**
      * --------------------------------------------------------------------------
